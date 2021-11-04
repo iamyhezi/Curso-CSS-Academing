b84275e850116694acd7d8b2bbb863612a9de26e
Imac <yhesling@hotmail.com>
2021-11-04 16:02:01 -0500

    9.7 Abriendo y cerrando el menú

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ var backdrop = document.querySelector(".backdrop");
 var modal = document.querySelector(".modal");
 var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
 // console.dir(backdrop);
 // console.dir(selectPlanButtons);
@@ -12,12 +14,21 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     });
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", function() {
+    mobileNav.style.display = "none";
+    this.closeModal();
+});
 
-modalNoButton.addEventListener("click, closeModal")
+modalNoButton.addEventListener("click", closeModal);
 
 function closeModal() {
     backdrop.style.display = "none";
     modal.style.display = "none";
 }
+
+toggleButton.addEventListener("click", function() {
+    mobileNav.style.display = "block"
+    backdrop.style.display = "block"
+});
+
 // backdrop.style.display = 'block';
