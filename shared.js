5d2cb53253e1d6b200d1ecfa0ee305c407828ffc
Imac <yhesling@hotmail.com>
2021-11-04 13:47:00 -0500

    9.4 Añadiendo eventos JS (var backdrop-modal)

--- shared.js
+++ shared.js
@@ -1,5 +1,14 @@
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
 
 // console.dir(backdrop);
+// console.dir(selectPlanButtons);
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener('click', function() {
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+    });
+}
 
-backdrop.style.display = 'block';
+// backdrop.style.display = 'block';
